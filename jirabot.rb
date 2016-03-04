8970ad3e9a02b5995c024e51f405d1078bce6619
Brad Hubbard <brad@bradhubbard.net>
2016-03-04 14:04:07 -0800

    Call JIRA and fetch issue details

--- jirabot.rb
+++ jirabot.rb
@@ -1,4 +1,7 @@
 require 'slack-ruby-bot'
+require "net/https"
+require "uri"
+require "JSON"
 
 class JiraBot < SlackRubyBot::Bot
   match(/([a-z]+-[0-9]+)/i) do |client, data, issues|
@@ -14,9 +17,29 @@ class JiraBot < SlackRubyBot::Bot
     # Now grab everything that looks like a JIRA ticket, dump it into an array, grab uniques.
     
     tomatch.scan(/([a-z]+-[0-9]+)/i) do |i,j|
-    	results << 'https://ticketfly.jira.com/browse/'+i.upcase
+    	results << i.upcase
     end
-    client.say(channel: data.channel, text: results.uniq.join("\n"))
+    results.uniq.each do |ticket|
+        url = ENV["JIRA_PREFIX"]+"rest/api/latest/issue/"+ticket
+        direct = ENV["JIRA_PREFIX"]+"browse/"+ticket
+        uri = URI.parse(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Get.new(uri.request_uri)
+        request.basic_auth(ENV["JIRA_USER"],ENV["JIRA_PASS"])
+        response = http.request(request)
+        body = JSON.parse(response.body)
+        if response.code == "200"
+            message = "#{direct}\n\n#{body['fields']['summary']}\n#{body['fields']['issuetype']['name']} (#{body['fields']['status']['name']})"
+        else
+            message = direct
+        end
+        client.say(channel: data.channel, text: message)
+    end
+
+
   end
 end
 
